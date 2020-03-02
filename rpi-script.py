90b50486246891067830cf4a8457dfe3dd547c03
root <nicolas.rodriguezcuellar@student.hu.nl>
2020-03-02 11:36:40 +0100

    new signal strength function and some clean up

--- rpi-script.py
+++ rpi-script.py
@@ -2,7 +2,7 @@ import hashlib
 from datetime import datetime
 
 import requests
-from scapy.layers.dot11 import Dot11ProbeReq, sniff
+from scapy.layers.dot11 import Dot11ProbeReq, sniff, RadioTap
 
 output = []
 
@@ -25,30 +25,30 @@ def hash_mac(plaintext: str) -> str:
 
 
 def get_signal_strength(pkt):
-    extra = pkt.notdecoded
-    rssi = -(256 - ord(extra[-4:-3]))  # this be magic
-    return str(rssi)
+    radiotap = pkt.getlayer(RadioTap)
+    rssi = radiotap.dBm_AntSignal
+    print(f"RSSI={rssi}")  # RSSI=-84
+    return rssi
 
 
 def handle_packet(pkt):
-    if not pkt.haslayer(Dot11ProbeReq):  # what is this condition
+    if not pkt.haslayer(Dot11ProbeReq):
         return
 
-    # this comment wat
-    if pkt.type == 0 and pkt.subtype == 4:  # subtype used to be 8 (APs) but is now 4 (Probe Requests)
+    if pkt.type == 0 and pkt.subtype == 4:  # filters out beacon requests
         unhashed_mac = pkt.addr2.upper()
-        if unhashed_mac:  # why would this not be
-            signal_strength = get_signal_strength(pkt)
-            mac = hash_mac(unhashed_mac)
-            debug = f"\033[95m Device MAC:{mac} - WiFi signal strength {signal_strength} \033[92m \033[0m"
-            print(debug)
-            output.append({"hash": mac, "strength": signal_strength})
+        signal_strength = get_signal_strength(pkt)
+        mac = hash_mac(unhashed_mac)
+
+        debug = f"\033[95m Device MAC:{mac} - WiFi signal strength {signal_strength} \033[92m \033[0m"
+        print(debug)
+        output.append({"hash": mac, "strength": signal_strength})
 
 
 def main():
     print(f" \n  \033[93m  Wifi Scanner Initialized  \033[0m  \n")
     while True:
-        sniff(iface="wlp2s0mon", prn=handle_packet, timeout=20)  # start sniffing
+        sniff(iface="wlan1mon", prn=handle_packet, timeout=60)  # start sniffing
         send_update()
 
 
