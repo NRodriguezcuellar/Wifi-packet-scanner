aa836a984d06e5db73fe678a6880424bd86b7770
nicolas <nicolas.rodriguezcuellar@student.hu.nl>
2020-03-30 17:00:09 +0200

    now clears the list after sending it to the gateway and added comment to explain how the sniffer works

--- RPI class.py
+++ RPI class.py
@@ -14,7 +14,7 @@ request_url = os.environ.get('REQUEST_URL')  # url for the request
 class RPI:  # class that represents the Raspberry Pi
 
     def __init__(self):
-        self.output = []
+        self.output = []  # hashed macs and signal strength information is kept before sending it out
 
     def handle_packet(self, pkt):  # function for getting the mac adress and signal strength from a probe request packet
         if not pkt.haslayer(Dot11ProbeReq):
@@ -29,7 +29,8 @@ class RPI:  # class that represents the Raspberry Pi
             self.output.append({"hash": mac, "strength": signal_strength})
 
     def sniffer(self, timeout=60, interface='wlan1mon'):
-
+        # activates the sniffer that actually scans for wifi packets and calls the
+        # handle_packet function that accepts that packet information as an argument
         sniff(iface=interface, prn=self.handle_packet, timeout=timeout)
 
     def send_update(self, apikey, url):  # function to send the scanned data to the gateway
@@ -41,6 +42,7 @@ class RPI:  # class that represents the Raspberry Pi
         headers = {"Authorization": f"Bearer {apikey}"}  # specify the api key in the header
         payload = {"data": self.output, "time": datetime.now().isoformat()}  # the data we scanned and hashed
         r = requests.post(url, headers=headers, json=payload)  # posting the request
+        self.output = []  # clears the list after it has been sent
 
         print(f"sent: {r.status_code}")
         # assert r.ok   4debug, add try except for timeout autism in prod
